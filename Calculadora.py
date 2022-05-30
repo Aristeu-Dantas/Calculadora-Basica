1cd4317527b12011053f8e79c75dfba807a2adf6
Aristeu Dantas <82111816+Aristeu-Dantas@users.noreply.github.com>
2022-05-29 21:38:02 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,14 +1,17 @@
 #Calculadora simples com as 4 operações
 
+#Definindo construtores
 numero1 = 0
 numero2 = 0
 resultado = 0
 operacao = ''
 
+#Recebendo info
 numero1 = int(input('Digite o número 1: '))
 operacao = input('Digite a operação: ')
 numero2 = int(input('Digite o número 2: '))
 
+#Aplicando lógica com laço e tratamento
 while True:
     try:
         if operacao == '+':
